e59ae0077c963579907a1f389e72eab6b26dbb79
greensTech67 <greensselenuiumtask@gmail.com>
2020-03-17 08:55:58 +0530

    MyNewCode

---
+++ src/cts/proj/modu/CourseDetails.java
@@ -0,0 +1,31 @@
+package cts.proj.modu;
+
+//Class
+public class CourseDetails {
+
+	// Methods
+	private void seleniumCourse() {
+
+		System.out.println("Seleium");
+
+	}
+
+	private void javaCourse() {
+		System.out.println("Java");
+
+	}
+
+	// Main Method
+	public static void main(String[] args) {
+		// Object >> Run Time Memory Allocation (RAM - Heap Memory)
+
+		// ClassName objectName/RefName = new ClassName();
+
+		CourseDetails object = new CourseDetails();
+
+		object.seleniumCourse();
+		object.javaCourse();
+
+	}
+
+}
---
+++ src/cts/proj/modu/Employee.java
@@ -0,0 +1,22 @@
+package cts.proj.modu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee {
+
+
+	
+
+	public static void main(String[] args) {
+
+		List<Integer> li = new ArrayList<Integer>();
+		
+		li.add(345);
+		
+		Integer i = li.get(0);
+		System.out.println(i);
+
+	}
+
+}
